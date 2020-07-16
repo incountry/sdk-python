68f6902d27ed4d188ab41afeddc6142045854eaf
vladimir.levitskiy <vladimir.levitsky@incountry.com>
2020-07-16 20:27:48 +0300

    SDK-160 remove deprecated code

--- setup.py
+++ setup.py
@@ -80,10 +80,6 @@ class UploadCommand(Command):
         self.status("Uploading the package to PyPI via Twine…")
         os.system("twine upload dist/*")
 
-        self.status("Pushing git tags…")
-        os.system("git tag v{0}".format(quote(about["__version__"])))
-        os.system("git push --tags")
-
         sys.exit()
 
 
