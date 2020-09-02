262eb6b90a0519c2df0bcc5ae52a889d70ed3c73
Roman Semin <roman.semin@incountry.com>
2020-09-02 19:54:14 +0300

    SDK-218 [FIX] type

--- incountry/storage.py
+++ incountry/storage.py
@@ -352,14 +352,14 @@ class Storage:
         """
         current_secret_version = self.crypto.get_current_secret_version()
         find_res = self.find(country=country, limit=limit, version={"$not": current_secret_version})
-        records_to_migrage_count = len(find_res["records"])
+        records_to_migrate_count = len(find_res["records"])
 
-        if records_to_migrage_count > 0:
+        if records_to_migrate_count > 0:
             self.batch_write(country=country, records=find_res["records"])
 
         return_data = {
-            "migrated": records_to_migrage_count,
-            "total_left": find_res["meta"]["total"] - records_to_migrage_count,
+            "migrated": records_to_migrate_count,
+            "total_left": find_res["meta"]["total"] - records_to_migrate_count,
         }
         if "errors" in find_res:
             return_data["errors"] = find_res["errors"]
